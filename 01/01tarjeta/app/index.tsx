6d3ef7020e34d6dab8fe02e8c4765b1d700e6385
andres.ojeda <andres.ojeda@iesnervion.es>
2025-10-07 11:15:33 +0200

    01 Tarjeta

---
+++ 01/01tarjeta/app/index.tsx
@@ -0,0 +1,42 @@
+// index.tsx
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const Card = () => {
+  return (
+    <View style={styles.card}>
+      <Image
+        source={{ uri: 'https://images.ctfassets.net/denf86kkcx7r/206sKHHdoJovwu9AzcdCQ/2b7b64112272693f8c3eeb4b491626b9/gato_negro_1_.jpg?fm=webp&w=612' }}
+        style={styles.image}
+      />
+      <Text style={styles.text}>Gatito  🐱</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    padding: 16,
+    margin: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 12,
+  },
+  text: {
+    marginTop: 12,
+    fontSize: 16,
+    color: '#333',
+    textAlign: 'center',
+  },
+});
+
+export default Card;
